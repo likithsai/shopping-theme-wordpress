d715f879b9516cd334bbd55b735a2150624b0122
likith sai <likithsai13@gmail.com>
2023-03-24 09:11:39 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+    Theme Name: New theme
+    Author: wp content
+    Author URI: https://wp-content.co/
+    Description: My custom theme
+    Version: 1.0
+    Tested upto: 5.4
+    */
+
+
+    echo "<p>sample example</p>";
+?>
