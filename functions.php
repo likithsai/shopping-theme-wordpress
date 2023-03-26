b81cc2cd76bcf3fec15d3bc2822dd68a39640f96
likith sai <likithsai13@gmail.com>
2023-03-26 12:32:36 +0530

    added required dbtables

--- functions.php
+++ functions.php
@@ -68,13 +68,15 @@
         function create_database_tables() {
             global $wpdb;
             $database_entries = [
-                "shopping" => "id mediumint(9) NOT NULL AUTO_INCREMENT, time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL, views smallint(5) NOT NULL, clicks smallint(5) NOT NULL, UNIQUE KEY id (id)",
-                "scasc" => "id mediumint(9) NOT NULL AUTO_INCREMENT, time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL, views smallint(5) NOT NULL, clicks smallint(5) NOT NULL, UNIQUE KEY id (id)"
+                "tbl_users" => "user_id mediumint(9) NOT NULL AUTO_INCREMENT, user_name varchar(200) DEFAULT NULL, user_email varchar(200) DEFAULT NULL, user_password varchar(200) NOT NULL, user_createddate timestamp NOT NULL DEFAULT current_timestamp(), PRIMARY KEY (user_id)",
+                "tbl_orders" => "order_id mediumint(9) NOT NULL AUTO_INCREMENT, order_name varchar(200) DEFAULT NULL, order_item varchar(200) DEFAULT NULL, order_cusid mediumint(9) DEFAULT NULL, order_price float DEFAULT NULL, order_createddate timestamp NOT NULL DEFAULT current_timestamp(), PRIMARY KEY (order_id), KEY talk_cusid (order_cusid), CONSTRAINT talk_cusid FOREIGN KEY (order_cusid) REFERENCES " . $wpdb -> prefix . "tbl_users(user_id) ON DELETE SET NULL ON UPDATE SET NULL",
+                "tbl_categories" => "category_id mediumint(9) NOT NULL AUTO_INCREMENT, category_name varchar(200) NOT NULL, category_createddate timestamp NOT NULL DEFAULT current_timestamp(), PRIMARY KEY (category_id)",
+                "tbl_products" => "product_id mediumint(9) NOT NULL AUTO_INCREMENT, product_name varchar(200) DEFAULT NULL, product_desc_short varchar(100) DEFAULT NULL, product_desc_long varchar(200) DEFAULT NULL, product_category_id mediumint(9) DEFAULT NULL, product_item_oldprice float DEFAULT NULL, product_item_newprice float DEFAULT NULL, product_createddate timestamp NOT NULL DEFAULT current_timestamp(), PRIMARY KEY (product_id), KEY talk_productid (product_id), CONSTRAINT talk_productcatid FOREIGN KEY (product_category_id) REFERENCES " . $wpdb -> prefix . "tbl_categories(category_id) ON DELETE SET NULL ON UPDATE SET NULL"
             ];
-            $charset_collate = $wpdb->get_charset_collate();
+            $charset_collate = $wpdb -> get_charset_collate();
 
             foreach($database_entries as $key => $value) {
-                $table_name = $wpdb->prefix . $key;
+                $table_name = $wpdb -> prefix . $key;
                 $sql = "CREATE TABLE IF NOT EXISTS $table_name($value)$charset_collate;"; 
                 dbDelta( $sql );
             }
